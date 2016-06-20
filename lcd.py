2b179f0cfdeb0e062c23237d81fc4048a9f78890
Brian J Gravelle <bgravelel5@gmail.com>
2016-06-20 22:33:20 +0000

    untested functions written

--- lcd.py
+++ lcd.py
@@ -16,8 +16,10 @@ import time
 
 #global objects? and variables
 bus = smbus.SMBus(1) # i2c bus
-LCD_ADDR = (0x3E>>1) # i2c address for lcd shifted to add R/W bit in smbus func
-RGB_ADDR = (0x62>>1) # i2c addr for rgb backlight
+LCD_ADDR = (0x7c>>1) # i2c address for lcd shifted to add R/W bit in smbus func
+RGB_ADDR = (0xc4>>1) # i2c addr for rgb backlight
+# LCD_ADDR = 0x3E        #alternatively
+# RGB_ADDR = 0x62   
 
 def lcd_init():
   # Initialise display
@@ -31,11 +33,49 @@ def lcd_init():
   lcd_write_cmd(0x07)   # increment (bit 1) entire shift (bit 0)
   time.sleep(0.002)     # 
 
+def lcd_write_string(str):
+   num = len(str)
+   i = 0
+   row = 0
+   while( (i < num) && (row < 2) ):
+      if( (i == 16) || (i == 32) || (str[i] == "\n") )
+         lcd_set_cursor(1, 0)
+         row++
+      lcd_write_data(ord(str[i++]))
+
+
+def lcd_set_rgb(r, g, b):
+   write_reg(0x00, 0x00)
+   write_reg(0x01, 0x00)
+   write_reg(0x08, 0xaa)   # set up whatever
+   write_reg(0x04, r)      # red
+   write_reg(0x03, g)      # green
+   write_reg(0x02, b)      # ocre (jk blue)
+
+def lcd_clear():
+   lcd_write_cmd(0x01)
+   time.sleep(0.002)
+
+def lcd_return_home():
+   lcd_write_cmd(0x02)
+   time.sleep(0.002)
+
+def lcd_set_cursor(row, col):
+   if(row == 0):
+      lcd_write_cmd(col | 0x80)
+   else:
+      lcd_write_cmd(col | 0xc0)
+   
+
+def lcd_clear():
+   lcd_write_cmd(0x01)
+   time.sleep(2)
 
 def lcd_write_cmd(cmd):
-   bus.write_byte(LCD_ADDR, 0x80)
-   bus.write_byte(LCD_ADDR, cmd)
+   bus.write_byte_data(LCD_ADDR, 0x80, cmd)
 
 def lcd_write_data(data):
-   bus.write_byte(LCD_ADDR, 0x40)
-   bus.write_byte(LCD_ADDR, data)
+   bus.write_byte_data(LCD_ADDR, 0x40, data)
+
+def lcd_write_reg(reg, data):
+   bus.write_byte_data(RGB_ADDR, reg, data)
